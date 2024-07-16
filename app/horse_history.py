57fa0250135c97c1738bc7ec451cc0e14d6a0696
Andy Chan <nikechung@gmail.com>
2024-07-16 10:07:55 +0800

    make code more structured

--- app/horse_history.py
+++ app/horse_history.py
@@ -2,179 +2,149 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 import numpy as np
 
-def getHorseHistory(filename):
+horse_history_data = None
+
+course_mapping = {
+    "A": 1,
+    "A+2": 2,
+    "A+3": 3,
+    "B": 4,
+    "B+2": 5,
+    "B+3": 6,
+    "C": 7,
+    "C+3": 8,
+    "NA": 9,
+    "NA+2": 10,
+    "NB": 11,
+    "NB+2": 12,
+    "AWT": 13
+}
+
+g_mapping = {
+    'G': 2.75,
+    'GF': 2.5,
+    'GD': 2.75,
+    'GY': 3,
+    'Y': 3.25,
+    'S': 3.25,
+    'WS': 3,
+    'FT': 2,
+    'WF': 2.5,
+    'YS': 3.5,
+    'H': 4
+}
+
+def getHorseHistory(filename, horse_data):
+    global horse_history_data
     horse_history_data = pd.read_csv(filename, parse_dates=['date'], date_format='%d/%m/%y')
     horse_history_data = horse_history_data.drop(horse_history_data[horse_history_data["finish_time"]=="--"].index)
     horse_history_data = horse_history_data.drop(horse_history_data[horse_history_data["weight"]=="--"].index)
-    return horse_history_data
+    
 
-def get_feature_mapping_table(feature, values, encoded_values):
-    mapping_table = pd.DataFrame({'Feature': feature, 'Value': values, 'Encoded Values': encoded_values})
-    mapping_table = mapping_table.drop_duplicates()
-    return mapping_table
+    horse_history_data["month"] = horse_history_data["date"].dt.month
 
-def map_feature_labelencoder(data, col_name):
-    le = LabelEncoder()
-    return le.fit_transform(data[col_name])
+    #process location_run
+    horse_history_data["course_location"] = horse_history_data["location_run"].apply(lambda x: x.split('/')[0].strip())
+    horse_history_data["track"] = horse_history_data["location_run"].apply(lambda x: x.split('/')[1].strip()) 
+    horse_history_data["course"] = horse_history_data["location_run"].apply(extractCourse)
+    
+    horse_history_data["weight"] = horse_history_data["weight"].astype("int")
 
-def concat_mapping_to_table(table, data, col_name, values):
-  return pd.concat([table, get_feature_mapping_table(col_name, data[col_name], values)], ignore_index=True)
+    horse_history_data["finish_time_ms"] = horse_history_data["finish_time"].apply(lambda x: None if x == "--" else (int(x.split(".")[0]) * 60 + int(x.split(".")[1])) * 1000 + int(x.split(".")[2]) * 10)
+    horse_history_data["speed_m_s"] = horse_history_data["distance"] / horse_history_data["finish_time_ms"] * 1000
+    horse_history_data["distance_km"] = horse_history_data["distance"].astype("int") / 1000
 
-def preProcess(horse_history_data, horse_data):
-    le = LabelEncoder()
-    horse_race_data = horse_history_data.copy()
-    feature_mapping_table = pd.DataFrame()
+    # Fill in horse information
+    horse_history_data["horse_country"] = horse_history_data["horse_id"].map(horse_data["country"].to_dict())
+    horse_history_data["horse_age"] = horse_history_data["horse_id"].map(horse_data["age"].to_dict())
+    horse_history_data["horse_age"] = horse_history_data["date"].dt.year - 2024 + horse_history_data["horse_age"]
+    horse_history_data["horse_owner"] = horse_history_data["horse_id"].map(horse_data["owner"].to_dict())
+    horse_history_data["horse_import_type"] = horse_history_data["horse_id"].map(horse_data["import_type"].to_dict())
+    horse_history_data["horse_color"] = horse_history_data["horse_id"].map(horse_data["color"].to_dict())
+    horse_history_data["horse_sex"] = horse_history_data["horse_id"].map(horse_data["sex"].to_dict())
+    horse_history_data["horse_sire"] = horse_history_data["horse_id"].map(horse_data["sire"].to_dict())
+    horse_history_data["horse_dam"] = horse_history_data["horse_id"].map(horse_data["dam"].to_dict())
+    horse_history_data["horse_dam_sire"] = horse_history_data["horse_id"].map(horse_data["dam_sire"].to_dict())
+    return horse_history_data.copy()
 
-    # Horse Race Details
-    # Race date
-    horse_race_data["month"] = horse_race_data["date"].dt.month
-    horse_race_data["quarter"] = pd.cut(horse_race_data["month"], bins=[0,3,6,9,12], labels=[1,2,3,4])
+def extractCourse(loc_run):
+    parts = loc_run.split('/')
+    if len(parts) >= 3:
+        course = parts[2].replace("\"", "").strip()
+    elif parts[1].strip() == 'AWT':
+        course = "AWT"
+    else:
+        course = None
+    return course
 
-    # location_run
-    horse_race_data["course_location"] = horse_race_data["location_run"].apply(lambda x: x.split('/')[0].strip())
-    horse_race_data["track"] = horse_race_data["location_run"].apply(lambda x: x.split('/')[1].strip())
+__labelEncoders = {}
+def __getLabelEncoder(field):
+    global __labelEncoders
+    if not field in __labelEncoders:
+        __labelEncoders[field] = LabelEncoder()
 
-    def extract_course(loc_run):
-        parts = loc_run.split('/')
-        if len(parts) >= 3:
-            course = parts[2].replace("\"", "").strip()
-        elif parts[1].strip() == 'AWT':
-            course = "AWT"
-        else:
-            course = None
-        return course
-    horse_race_data["course"] = horse_race_data["location_run"].apply(extract_course)
-    # horse_race_data["course"] = horse_race_data["location_run"].apply(lambda x:  x.split('/')[2].replace("\"", "").strip() if len(x.split('/')) >= 3 else None )
+    return __labelEncoders[field]
+    
+
+def encodeCourseLocation(course_location, fit=True):
+    global horse_history_data
+    le = __getLabelEncoder("course_location")
+    if fit:
+        le.fit(horse_history_data["course_location"])
+
+    return le.transform(course_location)
+    
+def encodeWithLabelEncoder(data, field, fit=True):
+    le = __getLabelEncoder(field)
+    if fit:
+        le.fit(data)
+    return le.transform(data) 
+
+def getMedianRank(field):
+    return horse_history_data.groupby(field)["speed_m_s"].median().rank()
+
+def encodeWithMedianRank(data, field):
+    global horse_history_data
+    return data.map(getMedianRank(field).to_dict())
+    
+def prepareData(race_data):
+    global horse_history_data
+    global course_mapping
+    
+    data = race_data.copy()
+
     # Remove foreign races
-    horse_race_data = horse_race_data[(horse_race_data["course_location"] == "ST") |(horse_race_data["course_location"] == "HV")]
-    # course_location mapping
-    course_location_values = map_feature_labelencoder(horse_race_data, "course_location")
-    feature_mapping_table = concat_mapping_to_table(feature_mapping_table, horse_race_data, "course_location", course_location_values)
-    horse_race_data["course_location"] = course_location_values
-    # track
-    # horse_race_data["track"] = horse_race_data["track"].astype('category')
-    track_values = map_feature_labelencoder(horse_race_data, "track")
-    feature_mapping_table = concat_mapping_to_table(feature_mapping_table, horse_race_data, "track", track_values)
-    horse_race_data["track"] = track_values
-    # course mapping
-    course_mapping = {
-        "A": 1,
-        "A+2": 2,
-        "A+3": 3,
-        "B": 4,
-        "B+2": 5,
-        "B+3": 6,
-        "C": 7,
-        "C+3": 8,
-        "NA": 9,
-        "NA+2": 10,
-        "NB": 11,
-        "NB+2": 12,
-        "AWT": 13
-    }
-    horse_race_data["course"] = horse_race_data["course"].map(course_mapping)
+    data = data[(data["course_location"] == "ST") | (data["course_location"] == "HV")]
 
-    # speed_m_s distance
-    horse_race_data["finish_time_ms"] = horse_race_data["finish_time"].apply(lambda x: None if x == "--" else (int(x.split(".")[0]) * 60 + int(x.split(".")[1])) * 1000 + int(x.split(".")[2]) * 10)
-    horse_race_data["speed_m_s"] = horse_race_data["distance"] / horse_race_data["finish_time_ms"] * 1000
-    horse_race_data["speed_class"] = pd.cut(horse_race_data["speed_m_s"], bins=[0,15,16,17,18,30])
-    horse_race_data["distance_km"] = horse_race_data["distance"].astype("int") / 1000
+    data["G"] = data["G"].map(g_mapping)
+    data["course"] = data["course"].map(course_mapping)
 
-    # G mapping
-    g_mapping = {
-        'G': 2.75,
-        'GF': 2.5,
-        'GD': 2.75,
-        'GY': 3,
-        'Y': 3.25,
-        'S': 3.25,
-        'WS': 3,
-        'FT': 2,
-        'WF': 2.5,
-        'YS': 3.5,
-        'H': 4
-    }
-    horse_race_data["G"] = horse_race_data["G"].map(g_mapping)
+    fieldsToEncodeWithMedian = ['race_class', 'dr', 'trainer', 'jockey', 'horse_country', 'horse_owner',
+                                'horse_import_type', 'horse_color', 'horse_sex', 
+                                'horse_sire', 'horse_dam', 'horse_dam_sire' , 
+                                'course']
 
-    # race_class
-    race_class_values = horse_race_data["race_class"].map(horse_race_data.groupby('race_class')["speed_m_s"].median().rank().to_dict())
-    feature_mapping_table = concat_mapping_to_table(feature_mapping_table, horse_race_data, "race_class", race_class_values)
-    horse_race_data["race_class"] = race_class_values
-    # dr
-    dr_values = horse_race_data["dr"].map(horse_race_data.groupby('dr')["speed_m_s"].median().rank().to_dict())
-    feature_mapping_table = concat_mapping_to_table(feature_mapping_table, horse_race_data, "dr", race_class_values)
-    horse_race_data["dr"] = dr_values
-    # trainer
-    trainer_values = horse_race_data["trainer"].map(horse_race_data.groupby('trainer')["speed_m_s"].median().rank().to_dict())
-    feature_mapping_table = concat_mapping_to_table(feature_mapping_table, horse_race_data, "trainer", trainer_values)
-    horse_race_data["trainer"] = trainer_values
-    # jockey
-    jockey_values = horse_race_data["jockey"].map(horse_race_data.groupby('jockey')["speed_m_s"].median().rank().to_dict())
-    feature_mapping_table = concat_mapping_to_table(feature_mapping_table, horse_race_data, "jockey", jockey_values)
-    horse_race_data["jockey"] = jockey_values
-    # Horse Weight (Declaration)
-    horse_race_data["weight"] = horse_race_data["weight"].astype("int")
-    horse_race_data["weight_class"] = pd.cut(horse_race_data["weight"], bins=[900, 950, 1000, 1050, 1100, 1150, 1200, 1250, 1300, 1350], labels=[0,1,2,3,4,5,6,7,8])
-    # Gear
-    horse_race_data["gear"] = horse_race_data["gear"].map(lambda x: 0 if x == "--" else 1)
+    for field in fieldsToEncodeWithMedian:
+        data[field] = encodeWithMedianRank(data[field], field)
 
-    # Horse Details
-    # Country of Origin
-    horse_race_data["horse_country"] = horse_race_data["horse_id"].map(horse_data["country"].to_dict())
-    country_values = horse_race_data["horse_country"].map(horse_race_data.groupby('horse_country')["speed_m_s"].median().rank().to_dict())
-    feature_mapping_table = concat_mapping_to_table(feature_mapping_table, horse_race_data, "horse_country", country_values)
-    horse_race_data["horse_country"] = country_values
-    # Age
-    horse_race_data["horse_age"] = horse_race_data["horse_id"].map(horse_data["age"].to_dict())
-    horse_race_data["horse_age"] = horse_race_data["date"].dt.year - 2024 + horse_race_data["horse_age"]
-    # Owner
-    horse_race_data["horse_owner"] = horse_race_data["horse_id"].map(horse_data["owner"].to_dict())
-    owner_values = horse_race_data["horse_owner"].map(horse_race_data.groupby('horse_owner')["speed_m_s"].median().rank().to_dict())
-    feature_mapping_table = concat_mapping_to_table(feature_mapping_table, horse_race_data, "horse_owner", owner_values)
-    horse_race_data["horse_owner"] = owner_values
-    # Import Type
-    horse_race_data["horse_import_type"] = horse_race_data["horse_id"].map(horse_data["import_type"].to_dict())
-    import_type_values = horse_race_data["horse_import_type"].map(horse_race_data.groupby('horse_import_type')["speed_m_s"].median().rank().to_dict())
-    feature_mapping_table = concat_mapping_to_table(feature_mapping_table, horse_race_data, "horse_import_type", import_type_values)
-    horse_race_data["horse_import_type"] = import_type_values
-    # Color
-    horse_race_data["horse_color"] = horse_race_data["horse_id"].map(horse_data["color"].to_dict())
-    color_values = horse_race_data["horse_color"].map(horse_race_data.groupby('horse_color')["speed_m_s"].median().rank().to_dict())
-    feature_mapping_table = concat_mapping_to_table(feature_mapping_table, horse_race_data, "horse_color", color_values)
-    horse_race_data["horse_color"] = color_values
+    data["gear"] = data["gear"].map(lambda x: 0 if x == "--" else 1)
+    data["track"] = encodeWithLabelEncoder(data["track"], 'track') 
+    
+    return data
+    
 
-    # Sex
-    horse_race_data["horse_sex"] = horse_race_data["horse_id"].map(horse_data["sex"].to_dict())
-    sex_values = horse_race_data["horse_sex"].map(horse_race_data.groupby('horse_sex')["speed_m_s"].median().rank().to_dict())
-    feature_mapping_table = concat_mapping_to_table(feature_mapping_table, horse_race_data, "horse_sex", sex_values)
-    horse_race_data["horse_sex"] = sex_values
-    # Sire
-    horse_race_data["horse_sire"] = horse_race_data["horse_id"].map(horse_data["sire"].to_dict())
-    sire_values = horse_race_data["horse_sire"].map(horse_race_data.groupby('horse_sire')["speed_m_s"].median().rank().to_dict())
-    feature_mapping_table = concat_mapping_to_table(feature_mapping_table, horse_race_data, "horse_sire", sire_values)
-    horse_race_data["horse_sire"] = sire_values
-    # Dam
-    horse_race_data["horse_dam"] = horse_race_data["horse_id"].map(horse_data["dam"].to_dict())
-    dam_values = horse_race_data["horse_dam"].map(horse_race_data.groupby('horse_dam')["speed_m_s"].median().rank().to_dict())
-    feature_mapping_table = concat_mapping_to_table(feature_mapping_table, horse_race_data, "horse_dam", dam_values)
-    horse_race_data["horse_dam"] = dam_values
-    # Dam Sire
-    horse_race_data["horse_dam_sire"] = horse_race_data["horse_id"].map(horse_data["dam_sire"].to_dict())
-    dam_sire_values = horse_race_data["horse_dam_sire"].map(horse_race_data.groupby('horse_dam_sire')["speed_m_s"].median().rank().to_dict())
-    feature_mapping_table = concat_mapping_to_table(feature_mapping_table, horse_race_data, "horse_dam_sire", dam_sire_values)
-    horse_race_data["horse_dam_sire"] = dam_sire_values
 
-    # horse_race_data["track"] = le.fit_transform(horse_race_data["track"])
-    # horse_race_data["course"] = horse_race_data["course"].map(horse_race_data.groupby('course')["speed_m_s"].median().rank().to_dict())
+# def preProcess_bak(horse_history_data, horse_data):
+#     horse_race_data = horse_history_data.copy()
 
-    # Drop features
-    col_to_drop = ["date", "result", "location_run", "running_position", "finish_time"]
-    horse_race_data = horse_race_data.drop(col_to_drop, axis=1)
+#     # Drop features
+#     col_to_drop = ["date", "result", "location_run", "running_position", "finish_time"]
+#     horse_race_data = horse_race_data.drop(col_to_drop, axis=1)
 
-    from scipy import stats
+#     from scipy import stats
 
-    # Calculate the Z-scores
-    z_scores = np.abs(stats.zscore(horse_race_data["speed_m_s"]))
-    outlier_mask = (z_scores > 3)
-    horse_race_data = horse_race_data[~outlier_mask]
-    return horse_race_data
+#     # Calculate the Z-scores
+#     z_scores = np.abs(stats.zscore(horse_race_data["speed_m_s"]))
+#     outlier_mask = (z_scores > 3)
+#     horse_race_data = horse_race_data[~outlier_mask]
+#     return horse_race_data
