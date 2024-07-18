653b26c069cd3b46bdfa0bcfe9bb6c065fc3fe48
AkiC6 <akichancnk@gmail.com>
2024-07-18 16:33:48 +0800

    add best hyperparams

--- app/algorithm_analysis.py
+++ app/algorithm_analysis.py
@@ -15,7 +15,7 @@ models = [("Linear Regression", LinearRegression()),
 
 def getModel():
     global models
-    name, model = models[5]
+    model = GradientBoostingRegressor(loss='huber', learning_rate=0.1, n_estimators=300, max_depth=3, subsample=0.8)
     return model
 
 def findBestAlgorithm(X, y):
@@ -43,6 +43,29 @@ def findBestHyperParameter(model, X, y):
         "n_estimators": [100, 200, 300],
         "max_depth": [3, 5, 7],
         "subsample": [0.1, 0.5, 1.0]
+    }
+
+    grid = GridSearchCV(estimator=model, param_grid=param_test, scoring='r2', n_jobs=-1, verbose=10)
+    grid.fit(X_train, y_train)
+    best_model = grid.best_estimator_
+    best_params = grid.best_params_
+    y_pred = best_model.predict(X_test)
+    score = r2_score(y_test, y_pred)
+    print("Best params: ", best_params)
+    print("Score: ", score)
+
+    return best_params, score
+
+def findBestHyperParameterFindTune(model, X, y):
+    # Best params:  {'learning_rate': 0.1, 'loss': 'huber', 'max_depth': 3, 'n_estimators': 300, 'subsample': 0.5}
+    # Score:  0.7809316995858007
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    param_test = {
+        # "learning_rate": [0.05, 0.1, 0.15],
+        # "n_estimators": [250, 300, 350],
+        # "max_depth": [2, 3, 4],
+        "subsample": [0.6, 0.7, 0.8, 0.9]
     }
 
     grid = GridSearchCV(estimator=model, param_grid=param_test, scoring='r2', n_jobs=-1, verbose=10)
